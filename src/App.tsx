b387e9043706a9cad1d58f73a5e49ffa0b29309d
AYRESRIO <AYRESRIO@HOTMAIL.COM>
2024-12-11 19:18:01 -0300

    header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
+import Header from "./components/Header";
 
 export default function App() {
   return (
     <>
-      <h1>MeuSite</h1>
+      <Header />
     </>
   );
 }
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,11 @@
+import "./styles.css";
+
+export default function Header() {
+  return (
+    <header className="ms-header">
+      <div className="ms-container test">
+        <h1>MeuSite</h1>
+      </div>
+    </header>
+  );
+}
